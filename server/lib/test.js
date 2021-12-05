adb500be3ee4292b0af979b7444d406dcd9c8baa
flets708 <flets708@gmail.com>
2021-12-05 21:17:32 +0900

    mod getFeedContents.js

--- server/lib/test.js
+++
@@ -1,8 +0,0 @@
-const { getOgImage } = require('./getFeedContents')
-
-const test = async () => {
-  const hoge = await getOgImage("https://www.shinnihon-ins.co.jp/industry-news/industry-news-20211203-7/")
-  console.log(hoge);
-}
-
-test()
